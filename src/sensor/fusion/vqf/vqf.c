21aef8c81418bf2d1360a2d736bb0d90f070ab40
Romain Dal Maso <romain.dalmaso@artefact2.com>
2026-03-24 12:28:13 +0100

    Set magnetometer ODR to 10Hz
    The default 100Hz is overkill for slight yaw compensations over long
    periods of time.
    
    Using 10Hz on QMC6309 saves 270uA of power.
    
    To compensate for higher magnetometer noise, increase magCurrentTau.

--- src/sensor/fusion/vqf/vqf.c
+++ src/sensor/fusion/vqf/vqf.c
@@ -130,8 +130,7 @@ static void set_params()
 	params.restThAcc = 0.20f;
 	params.restThGyr = 0.65f;
 	params.magDistRejectionEnabled = true;
-	params.tauMag = 12.0f;
-	params.magCurrentTau = 0.20f;
+	params.magCurrentTau = 0.5f;
 	params.magNormTh = 0.09f;
 	params.magDipTh = 9.0f;
 	params.magNewFirstTime = 6.0f;
